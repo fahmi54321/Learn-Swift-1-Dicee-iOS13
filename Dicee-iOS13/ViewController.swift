ee9cdacdb627f0eaa27baa7316de41f4f9eea7ae
fahmi <fahmiabdulaziz44@gmail.com>
2023-05-28 20:25:08 +0700

    test

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOulted untuk ganti properties ui
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 6
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView1.alpha = 0.75
+        
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        diceImageView2.alpha = 0.5
     }
-
-
+    
+    // IBAction untuk aksi setiap element
+    @IBAction func roolButtnPressed(_ sender: UIButton) {
+        
+        print("tapped")
+        
+        // change ui element
+//        diceImageView1.image = UIImage(named: "DiceOne")
+//        diceImageView1.alpha = 1.0
+//
+//        diceImageView2.image = UIImage(named: "DiceThree")
+//        diceImageView2.alpha = 1.0
+          
+          // mutable variable
+//        var diceArray = [
+//            UIImage(named: "DiceOne"),
+//            UIImage(named: "DiceTwo"),
+//            UIImage(named: "DiceThree"),
+//            UIImage(named: "DiceFour"),
+//            UIImage(named: "DiceFive"),
+//        ]
+        
+        // constant variable
+        let diceArray = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+        ]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+//        leftDiceNumber = leftDiceNumber + 1
+//        rightDiceNumber = rightDiceNumber -1
+    }
+    
 }
 
